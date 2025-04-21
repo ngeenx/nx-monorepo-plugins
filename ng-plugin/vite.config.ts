4e6a81cde15807b5c5d918a225b66b444d8423df
Eyüp <epsilonelittr@gmail.com>
2025-04-21 22:34:59 +0300

    Update root folder paths

--- ng-plugin/vite.config.ts
+++ ng-plugin/vite.config.ts
@@ -4,7 +4,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/tools/ng-plugin',
+  cacheDir: '../../../node_modules/.vite/tools/ng-plugin',
   plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
@@ -17,7 +17,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/tools/ng-plugin',
+      reportsDirectory: '../../../coverage/tools/ng-plugin',
       provider: 'v8',
     },
   },
--- nx-plugin/vite.config.ts
+++ nx-plugin/vite.config.ts
@@ -4,7 +4,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/tools/nx-plugin',
+  cacheDir: '../../../node_modules/.vite/tools/nx-plugin',
   plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
@@ -17,7 +17,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/tools/nx-plugin',
+      reportsDirectory: '../../../coverage/tools/nx-plugin',
       provider: 'v8',
     },
   },
